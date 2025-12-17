f1e240d082a185ac6065f55c402cc632fea12507
matteolpera <matteo.lepera02@gmail.com>
2025-12-17 15:07:55 +0100

    feat: aggiunte operazioni crud: index show store update modify delete

--- routers/posts.js
+++ routers/posts.js
@@ -1,22 +1,32 @@
 import express from "express";
-import { ferrariWins } from "./data.js";
+import { ferrariWins } from "../data.js";
 
 const router = express.Router();
 
 //INDEX
-router.("", (req, res) => {
-
+router.get("/", (req, res) => {
+    res.send("Lista vittorie ferrari")
 })
 //SHOW
-
+router.get("/:id", (req, res) => {
+    res.send("Singola vittoria ferrari")
+})
 //STORE
-
+router.post("/", (req, res) => {
+    res.send("Aggiungi una vittoria ferrari")
+})
 //UPDATE
-
+router.put("/:id", (req, res) => {
+    res.send("Modifica una vittoria ferrari")
+})
 //MODIFY
-
+router.patch("/:id", (req, res) => {
+    res.send("Modifica dettagli di una vittoria ferrari")
+})
 //DESTROY
-
+router.delete("/:id", (req, res) => {
+    res.send("Elimina una vittoria ferrari")
+})
 
 
 
