11c7652d57d3725fdc71ed27478e5a8595c4080c
matteolpera <matteo.lepera02@gmail.com>
2025-12-17 14:43:23 +0100

    feat: aggiunta scaletta crud

--- routers/posts.js
+++ routers/posts.js
@@ -3,6 +3,21 @@ import { ferrariWins } from "./data.js";
 
 const router = express.Router();
 
+//INDEX
+router.("", (req, res) => {
+
+})
+//SHOW
+
+//STORE
+
+//UPDATE
+
+//MODIFY
+
+//DESTROY
+
+
 
 
 export default router;
